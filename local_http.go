5fff9967ab2205129173d0a9850e6aabd289b57b
cocgo <521401@qq.com>
2015-03-27 17:20:49 +0800

    create local http server

---
+++ local_http.go
@@ -0,0 +1,47 @@
+﻿package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strconv"
+)
+
+var dir string
+var port int
+var staticHandler http.Handler
+
+// 初始化参数
+func init() {
+	dir = path.Dir(os.Args[0])
+	flag.IntVar(&port, "port", 80, "服务器端口")
+	flag.Parse()
+	staticHandler = http.FileServer(http.Dir(dir))
+}
+
+func main() {
+	fmt.Println("zhangliangzhi  2015-03-27 17:19:40  521401@qq.com")
+	fmt.Println(os.Args[0])
+	fmt.Println("http server start ok...")
+
+	http.HandleFunc("/", StaticServer)
+	err := http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+}
+
+// 静态文件处理
+func StaticServer(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		staticHandler.ServeHTTP(w, req)
+		return
+	}
+
+	io.WriteString(w, "hello, update!\n")
+}
